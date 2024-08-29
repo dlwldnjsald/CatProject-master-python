d907ec52c501644bde7bb3d6c58cb6a6b75218cf
dlwldnjsald <163090571+dlwldnjsald@users.noreply.github.com>
2024-08-29 12:57:28 +0900

    0829
    0829

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-/*
-document.getElementById('catButton').addEventListener('click', async function() {
-    const response = await fetch('https://api.thecatapi.com/v1/images/search');
-    const data = await response.json();
-
-    const catImage = document.getElementById('catImage');
-    const breedInfo = document.getElementById('breedInfo');
-
-    if (data[0]) {
-        const imageUrl = data[0].url;
-        const breed = data[0].breeds.length > 0 ? data[0].breeds[0] : null;
-
-        catImage.src = imageUrl;
-        breedInfo.textContent = breed ? `${breed.name}: ${breed.temperament}` : 'Breed information is not available.';
-    }
-});
-
-*/
 document.getElementById('catButton').addEventListener('click', async function() {
     try {
         const response = await fetch('https://api.thecatapi.com/v1/images/search');
